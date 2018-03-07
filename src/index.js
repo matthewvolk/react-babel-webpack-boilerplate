76c99b396043c095ed4caeff985f99bd13696b33
matthewvolk <volkmattj@gmail.com>
2018-03-06 23:35:55 -0800

    first commit

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const Title = () => (
+  <div>'My Minimal React Webpack Babel Setup'</div>
+);
+
+ReactDOM.render(
+  <Title />,
+  document.getElementById('app')
+);
